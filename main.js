40eb1c012a6331315e0c5946bfc0f31b26cb95df
Andrew Hanks <andymchanks@gmail.com>
2023-08-29 15:44:29 -0400

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,14 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+for(var i = 0; i<cohortOneStudents.length; i++) {
+    console.log(cohortOneStudents[i])
+}
+
+var  myInterests = ["D & D", "Coding!", "Fitness","Rafting", "Strategy Games"]
+
+for(var i = 0; i < myInterests.length; i++) {
+    console.log(`One of my interests is: ${myInterests[i]}.`);
+  }
+
+  if(myInterests['1']) {
+    console.log(`My absolute favorite interest is: ${myInterests['1']}`)
+  }
